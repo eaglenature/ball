c3c132566b5312bfdd0c534cf135d941252068da
eaglenature <eaglenature@gmail.com>
2013-09-22 17:49:20 +0200

    Insertion sort

---
+++ ball/insertion_sort.h
@@ -0,0 +1,33 @@
+/*
+ * insertion_sort.h
+ *
+ *  Created on: Sep 22, 2013
+ *      Author: eaglenature@gmail.com
+ */
+
+#ifndef INSERTION_SORT_H_
+#define INSERTION_SORT_H_
+
+
+namespace ball
+{
+
+template<class InputIterator>
+void insertion_sort(InputIterator first, InputIterator last)
+{
+    typedef typename std::iterator_traits<InputIterator>::value_type value_type;
+    auto distance = std::distance(first, last);
+    for (auto curr = 0; curr < distance; ++curr)
+    {
+        auto k = curr;
+        while (k > 0 && std::less<value_type>()(first[k], first[k-1]))
+        {
+            std::swap(first[k], first[k-1]);
+            --k;
+        }
+    }
+}
+
+} // namespace ball
+
+#endif /* INSERTION_SORT_H_ */
